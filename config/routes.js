a11b5ae1e1acdb5813a63fd6a3a085e863930683
Bao Pham <ryanvuepham@gmail.com>
2019-04-06 12:45:44 -0500

    register user is working

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,56 @@
-const axios = require('axios');
+const axios = require("axios")
 
-const { authenticate } = require('../auth/authenticate');
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const { authenticate } = require("../auth/authenticate")
+const secrets = require("../secrets/secret").jwtSecret
+const db = require("../database/dbConfig.js")
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
-};
+  server.post("/api/register", register)
+  server.post("/api/login", login)
+  server.get("/api/jokes", authenticate, getJokes)
+}
 
-function register(req, res) {
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: "45m" // m = minute(s)
+  }
+  return jwt.sign(payload, secrets, options)
+}
+
+async function register(req, res) {
   // implement user registration
+  const creds = req.body
+  hash = bcrypt.hashSync(creds.password, 10)
+  creds.password = hash
+
+  try {
+    const ids = await db("users").insert(creds)
+    const id = ids[0]
+    const user = await db("users")
+      .where({ id })
+      .first()
+    const token = generateToken(user)
+    res.status(201).json({ token })
+  } catch (err) {
+    if (err.errno === 19) {
+      res
+        .status(409)
+        .json({ message: "That username is taken, please try another!" })
+    } else {
+      console.log(err)
+      res
+        .status(500)
+        .json({ message: "Something went wrong, please try again." })
+    }
+  }
 }
 
 function login(req, res) {
@@ -18,15 +59,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
-  };
+    headers: { accept: "application/json" }
+  }
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
-      res.status(200).json(response.data.results);
+      res.status(200).json(response.data.results)
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err })
+    })
 }
---
+++ secrets/secret.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || "Kepp calm, you are are not authorized"
+}
